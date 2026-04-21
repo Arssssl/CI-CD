729a5daa0aaaa99ab24b3cb1a508636d131eb340
Arssssl <136087735+Arssssl@users.noreply.github.com>
2026-04-22 04:30:07 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Deployed Version is v2.0. Hafiz Arslan latif</h1>");
+  response.end("<h1> Deployed Version is v2.0. Hafiz Arslan latif in 2k26</h1>");
 
 }
 var www = http.createServer(handleRequest);
